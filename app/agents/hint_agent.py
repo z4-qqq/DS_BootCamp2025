f44391b6bbd57ff12d53a887f4bdfce431f61b56
z4-qqq <allvisionk@mail.ru>
2025-04-19 00:20:38 +0300

    Merge main

---
+++ app/agents/hint_agent.py
@@ -0,0 +1,11 @@
+from agents import Agent
+from app.agents.tools.lie_answer import lie_answer
+from app.agents.tools.context import context
+
+def create_interviewee_agent(system_prompt: str) -> Agent:
+    return Agent(
+        name="Агент для генерации подсказак нанимающему менеджеру",
+        handoff_description="Ты умный помощник, который помогает придумывать вопросы таким образом, чтобы раскрыть компетенцию кандидата",
+        instructions=system_prompt,
+        tools=[lie_answer, context],
+    )
