41e95c0fa7488837bfa6aa54221815804f8e1988
Valery <valery.bazyrov@raiffeisen.ru>
2025-04-18 20:49:47 +0300

    add simple authorization

--- app/main.py
+++ app/main.py
@@ -1,32 +1,24 @@
+import hashlib
 from datetime import datetime, timedelta
 
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from jose import jwt
-from passlib.context import CryptContext
 
 from app.api.evaluation import router as evaluation_router
 from app.api.interview import router as interview_router
 
 app = FastAPI()
 
-fake_users_db = {
-    "user@example.com": {
-        "username": "user@example.com",
-        "hashed_password": "$2b$12$UU7U9I2J8PiBFu1qnb0l6.QmY28rqMTj03dzDew1FwEtMSVRnUzXa",  # lantenak
-    }
-}
+app.mount("/frontend", StaticFiles(directory="app/frontend"), name="frontend")
 
-SECRET_KEY = "your-secret-key"
+simple_db = {"user": hashlib.sha256("pass".encode()).hexdigest()}
+
+SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain, hashed):
-    return pwd_context.verify(plain, hashed)
 
 
 # Инициализируем роутеры для API
@@ -50,19 +42,20 @@ async def login_page(request: Request):
 
 @app.post("/api/login")
 async def login(username: str = Form(...), password: str = Form(...)):
-    user = fake_users_db.get(username)
-    if not user or not verify_password(password, user["hashed_password"]):
-        return JSONResponse(status_code=401, content={"error": "Invalid credentials"})
-
-    access_token = jwt.encode(
-        {
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    stored_password = simple_db.get(username)
+    if stored_password and stored_password == hashed_password:
+        payload = {
             "sub": username,
             "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
-        },
-        SECRET_KEY,
-        algorithm=ALGORITHM,
-    )
-    return {"access_token": access_token}
+        }
+        access_token = jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
+        return {"access_token": access_token}
+    else:
+        return JSONResponse(
+            status_code=401,
+            content={"error": "Неверный логин или пароль"},
+        )
 
 
 @app.get("/select-persona", response_class=HTMLResponse)
