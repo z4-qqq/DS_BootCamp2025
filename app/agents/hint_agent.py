859a97ac7e37e2ba00a017ce072cc72c3cf38ea0
z4-qqq <allvisionk@mail.ru>
2025-04-19 02:08:35 +0300

    Added hints agent

--- app/agents/hint_agent.py
+++ app/agents/hint_agent.py
@@ -2,10 +2,9 @@ from agents import Agent
 from app.agents.tools.lie_answer import lie_answer
 from app.agents.tools.context import context
 
-def create_interviewee_agent(system_prompt: str) -> Agent:
+def create_hint_agent(system_prompt: str) -> Agent:
     return Agent(
         name="Агент для генерации подсказак нанимающему менеджеру",
         handoff_description="Ты умный помощник, который помогает придумывать вопросы таким образом, чтобы раскрыть компетенцию кандидата",
         instructions=system_prompt,
-        tools=[lie_answer, context],
     )
