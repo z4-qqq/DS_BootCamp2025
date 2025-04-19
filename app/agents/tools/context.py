c5002d69b4abe32b3aec1df21d4356650650f616
z4-qqq <allvisionk@mail.ru>
2025-04-19 04:29:20 +0300

    Added new evaluation based on STAR

--- app/agents/tools/context.py
+++ app/agents/tools/context.py
@@ -6,3 +6,10 @@ async def context(ctx: RunContextWrapper[None]) -> list[dict[str, str]]:
 
     history = ctx.context.get("chat_history", [])
     return history
+
+@function_tool
+async def skill(ctx: RunContextWrapper[None]) -> str:
+    """Использует скилл для оценки по SMART"""
+
+    skill = ctx.context.get("skill", "")
+    return skill
